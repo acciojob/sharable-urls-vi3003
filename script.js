48d0df9cc73be429c87d5ac43f8478d40805cf4c
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-02-14 21:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+let name = document.getElementById("name")
+let year = document.getElementById("year")
+let click = document.getElementById("button")
+let title = document.getElementById("url")
+
+function titleUpdate(event) {
+	event.preventDefault();
+	if(name.value === "" && year.value !== ""){
+		title.innerHTML = "https://localhost:8080/?" + "year=" + year.value
+	}else if(name.value !== "" && year.value === ""){
+		title.innerHTML = "https://localhost:8080/?" + "name=" + name.value
+	}else{
+		title.innerHTML = "https://localhost:8080/?"  + "name=" + name.value + "&year=" + year.value
+	}
+}
